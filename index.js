d7992de5df56f9e3a7f2db956cc140f62c8fd1e7
George Toppo <gtoppo99@gmail.com>
2020-08-24 08:17:39 +0530

    Fixed support discord.js v12

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ class MongoDBProvider extends SettingProvider {
 			this.settings.set(guild, doc.settings);
 
 			// Guild is not global, and doesn't exist currently so lets skip it.
-			if (guild !== 'global' && !client.guilds.cache.cache.has(doc.guild)) return;
+			if (guild !== 'global' && !client.guilds.cache.has(doc.guild)) return;
 
 			this.setupGuild(guild, doc.settings);
 		});
